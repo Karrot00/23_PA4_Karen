a38064ae3b4ae4d914c5b948ad32be3a889aad28
Raullow <64115106+Raullow@users.noreply.github.com>
2020-04-30 14:14:15 +0800

    Updated Unity Loading Screen - Raul

---
+++ 23_Karen_PA4/Assets/Scripts/Title.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DoChangeScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public float delay = 1;
+
+    /*public void ChangeToScene(string CubeScene)
+    {
+        StartCoroutine(DoChangeScene(CubeScene, 1f));
+    }*/
+    IEnumerator DoChangeScene()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1);
+    }
+}
